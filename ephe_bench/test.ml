168840cd8d78e3897cd86054d9d3ab18af5590ec
Olivier Nicole <olivier@chnik.fr>
2026-03-12 17:36:39 +0100

    Benchmark

---
+++ ephe_bench/test.ml
@@ -0,0 +1,30 @@
+module E = Ephemeron.K1
+(*
+module E = struct
+  type ('a, 'b) t = 'a * 'b
+
+  let make k v = (k, v)
+end
+*)
+
+let size = 50_000
+
+let keys = Array.init size ref
+
+type tree = Node of (int ref, tree) E.t list
+
+let rec mk n : tree =
+  if n = 0 then Node []
+  else
+    let e = E.make keys.(n) (mk (n - 1)) in
+    Node [e]
+
+let () =
+  let tree = Array.init 10 (fun _ -> mk (size / 10)) in
+  for _ = 1 to 10 do
+    Gc.major ();
+    Printf.printf ".%!";
+  done;
+  ignore (Sys.opaque_identity tree);
+  print_newline ();
+;;
